d2695af100dfd4d46b05665e1eda3ea770f89f71
Saba <64770978+github-saba@users.noreply.github.com>
2026-04-20 00:26:46 +0100

    Add initial App.xaml.cs for application logic

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace Telecom-Prefix-Locator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Can be overriden OnStartup here if you need to 
+        // perform logic before the first window opens.
+    }
+}
